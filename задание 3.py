6f868a42e5c352bf6687793309bc922929788490
alyonaegorova <74093599+alyonaegorova@users.noreply.github.com>
2020-11-27 13:40:55 +0300

    Add files via upload

---
+++ задание 3.py
@@ -0,0 +1,23 @@
+1.
+l1 = [1, 2, 3]
+l2 = [i * 2 for i in l1]
+print(l2)
+
+2.
+l1 = [1, 2, 3]
+res = 0
+for num in l1:
+    res += num ** 2
+print(res)
+
+3.
+s = 'Hello world'
+if "" in s:
+    s = s.upper()
+else:
+    s = s.lower()
+
+print(s)
+
+
+
